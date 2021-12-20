17939aaf88438b3c12f06ff5f989a21386accbb1
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-12-21 01:11:51 +0530

    made login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { AuthProvider } from './context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import { routes } from './utils/routeStrings';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.HOME} element={<Home />} />
-        <Route path={routes.LOGIN} element={<Login />} />
-        <Route path="*" element={<Navigate to={routes.LOGIN} />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.HOME} element={<Home />} />
+          <Route path={routes.LOGIN} element={<Login />} />
+          <Route path="*" element={<Navigate to={routes.LOGIN} />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react"
+import { useAuth } from "../context/AuthContext"
+
+
 function Login() {
+
+  const [error, setError] = useState<string | null>(null)
+
+  const authContext = useAuth()
+
+  async function loginHandler() {
+
+    try {
+      setError(null)
+      const user = await authContext?.login()
+
+      console.log(user);
+
+    } catch (err) {
+      setError("Login Failed. please try again")
+      console.log(err);
+    }
+
+
+
+  }
 
   return (
     <div className="w-screen h-screen flex items-center justify-center" >
+      {/**
+       * make a error part that always exist but opacity is 0
+       * when error is null opacity 0 and pointer events all
+       * when error then opacity 1 with transition, red border and error in between
+      */}
       <div className="p-5 border rounded-lg shadow-sm">
         <h1 className="text-gray-800 " >Login to <span className="font-bold" >Expense Tracker</span></h1>
+
+        <button
+          className="font-bold text-amber-300 border border-amber-300 p-3 rounded-lg w-full mt-4 hover:bg-amber-300 hover:text-white transition-colors duration-200 "
+          type="button"
+          onClick={loginHandler}
+        >
+          Login with Google
+        </button>
       </div>
     </div>
   )
