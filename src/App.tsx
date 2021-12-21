5b70ef447ea4d21e2a30995eb44575acc86606bb
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-12-21 11:46:39 +0530

    handled authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import { routes } from './utils/routeStrings';
@@ -10,13 +10,39 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={routes.HOME} element={<Home />} />
-          <Route path={routes.LOGIN} element={<Login />} />
+          <Route path={routes.HOME} element={
+            <RequireAuth redirectTo={routes.LOGIN} >
+              <Home />
+            </RequireAuth>
+          } />
+          <Route path={routes.LOGIN} element={
+            <NoAuth redirectTo={routes.HOME} >
+              <Login />
+            </NoAuth>
+          } />
           <Route path="*" element={<Navigate to={routes.LOGIN} />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
   );
+}
+
+interface RouteElementWrapperPropTypes {
+  children: JSX.Element
+  redirectTo: string
+}
+
+function RequireAuth({ children, redirectTo }: RouteElementWrapperPropTypes) {
+
+  const authContext = useAuth()
+
+  return authContext?.currentUser ? children : <Navigate to={redirectTo} />
+}
+
+function NoAuth({children, redirectTo }: RouteElementWrapperPropTypes) {
+  const authContext = useAuth()
+
+  return authContext?.currentUser ? <Navigate to={redirectTo} /> : children
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import { useAuth } from "../context/AuthContext"
+
 function Home() {
+  const authContext = useAuth()
 
   return (
     <div>
       This is home.
+      <button
+        className="border border-red-500 text-red-500 p-2 rounded-sm m-1 hover:bg-red-400 hover:text-white transition-colors duration-200 ease-in-out"
+        type="button"
+        onClick={() => authContext?.logout()}
+      >
+        Logout
+      </button>
     </div>
   )
 }
