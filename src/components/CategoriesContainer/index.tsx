fae274745d31a161d9306eb59c86dcc5c158d4ca
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-12-23 00:07:08 +0530

    fetch call to get all the categories

--- src/components/CategoriesContainer/index.tsx
+++ src/components/CategoriesContainer/index.tsx
@@ -1,149 +1,185 @@
+import { useEffect, useState } from "react"
 import Category from "./Category"
 import { category } from "../../types/category.types"
+import { useAuth } from "../../context/AuthContext"
 
-const categories: category[] = [
-  {
-    "id": 12,
-    "created": 1638380190465,
-    "modified": 1638380190465,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "rashan",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1604719312566-8912e9227c6a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxMHx8Z3JvY2VyaWVzfGVufDB8fHx8MTYzODM4MDE4MA&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z6WdsrJ_PSxNS7zxQ_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 13,
-    "created": 1638380224184,
-    "modified": 1638380224184,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "personal",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw3fHxwZXJzb25hbHxlbnwwfHx8fDE2MzgzODAyMTU&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z6WdBNI_qaLHAqpxp_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 14,
-    "created": 1638594968706,
-    "modified": 1638594968706,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "evening snacks",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1613919113640-25732ec5e61f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw5MXx8c25hY2tzfGVufDB8fHx8MTYzODU5NDk1MQ&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7eLN56_OVSiNN27V_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 15,
-    "created": 1638595114497,
-    "modified": 1638595114497,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "college related",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwyfHxzdHVkeXxlbnwwfHx8fDE2Mzg1OTUxMTA&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7eMtyV_JG70onewG_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 16,
-    "created": 1638680559480,
-    "modified": 1638680559480,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "breakfast",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxNnx8YnJlYWtmYXN0fGVufDB8fHx8MTYzODY4MDU0Nw&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7lo4i0_2nSqU7UBQ_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 18,
-    "created": 1638990187552,
-    "modified": 1638990187552,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "dinner",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1463183547458-6a2c760d0912?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHxkaW5uZXJ8ZW58MHx8fHwxNjM4OTkwMTgw&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7Jhw5Q_GW2FnVskD_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 19,
-    "created": 1639048259601,
-    "modified": 1639048259601,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "Vegetables",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1566385101042-1a0aa0c1268c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw4fHx2ZWdldGFibGVzfGVufDB8fHx8MTYzOTA0ODI0OQ&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7NKndl_dIAbxcFDV_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 17,
-    "created": 1638780333487,
-    "modified": 1638780333487,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "room related",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1489171078254-c3365d6e359f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw4fHxyb29tJTIwcmVudHxlbnwwfHx8fDE2Mzg3ODAzMjA&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z7t5Xi7_GeJzssaA9_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 23,
-    "created": 1639566594021,
-    "modified": 1639566594021,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "transportation",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1473042904451-00171c69419d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHx0cmFuc3BvcnR8ZW58MHx8fHwxNjM5NTY2NTg2&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z8tK50l_vloq9u1ev_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 25,
-    "created": 1639760504792,
-    "modified": 1639760504792,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "entertainment",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1499364615650-ec38552f4f34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHxlbnRlcnRhaW5tZW50fGVufDB8fHx8MTYzOTc2MDQ5Nw&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z8IHN6w_gQynsdQOr_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 26,
-    "created": 1639983905450,
-    "modified": 1639983905450,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "gas cylinder",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1511973774220-37fab967aba0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwyM3x8Z2FzJTIwY3lsaW5kZXJ8ZW58MHx8fHwxNjM5OTgzODY5&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z91W4OO_RVzwOhU1h_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 27,
-    "created": 1639984231871,
-    "modified": 1639984231871,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "pani jar",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1618513631742-747b74aa48f4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHx3YXRlciUyMGphcnxlbnwwfHx8fDE2Mzk5ODQyMDI&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z91Xzvb_bUWTD3hdk_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  },
-  {
-    "id": 28,
-    "created": 1639984350166,
-    "modified": 1639984350166,
-    "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
-    "categoryName": "lunch",
-    "categoryDescription": "",
-    "imageUrl": "https://images.unsplash.com/photo-1546793665-c74683f339c1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwzfHxsdW5jaHxlbnwwfHx8fDE2Mzk5ODQzMTg&ixlib=rb-1.2.1&q=80&w=1080",
-    "key": "z9208mK_MCMuDdpO7_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
-  }
-]
+// const categories: category[] = [
+//   {
+//     "id": 12,
+//     "created": 1638380190465,
+//     "modified": 1638380190465,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "rashan",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1604719312566-8912e9227c6a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxMHx8Z3JvY2VyaWVzfGVufDB8fHx8MTYzODM4MDE4MA&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z6WdsrJ_PSxNS7zxQ_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 13,
+//     "created": 1638380224184,
+//     "modified": 1638380224184,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "personal",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw3fHxwZXJzb25hbHxlbnwwfHx8fDE2MzgzODAyMTU&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z6WdBNI_qaLHAqpxp_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 14,
+//     "created": 1638594968706,
+//     "modified": 1638594968706,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "evening snacks",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1613919113640-25732ec5e61f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw5MXx8c25hY2tzfGVufDB8fHx8MTYzODU5NDk1MQ&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7eLN56_OVSiNN27V_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 15,
+//     "created": 1638595114497,
+//     "modified": 1638595114497,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "college related",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwyfHxzdHVkeXxlbnwwfHx8fDE2Mzg1OTUxMTA&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7eMtyV_JG70onewG_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 16,
+//     "created": 1638680559480,
+//     "modified": 1638680559480,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "breakfast",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxNnx8YnJlYWtmYXN0fGVufDB8fHx8MTYzODY4MDU0Nw&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7lo4i0_2nSqU7UBQ_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 18,
+//     "created": 1638990187552,
+//     "modified": 1638990187552,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "dinner",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1463183547458-6a2c760d0912?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHxkaW5uZXJ8ZW58MHx8fHwxNjM4OTkwMTgw&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7Jhw5Q_GW2FnVskD_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 19,
+//     "created": 1639048259601,
+//     "modified": 1639048259601,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "Vegetables",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1566385101042-1a0aa0c1268c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw4fHx2ZWdldGFibGVzfGVufDB8fHx8MTYzOTA0ODI0OQ&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7NKndl_dIAbxcFDV_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 17,
+//     "created": 1638780333487,
+//     "modified": 1638780333487,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "room related",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1489171078254-c3365d6e359f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHw4fHxyb29tJTIwcmVudHxlbnwwfHx8fDE2Mzg3ODAzMjA&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z7t5Xi7_GeJzssaA9_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 23,
+//     "created": 1639566594021,
+//     "modified": 1639566594021,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "transportation",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1473042904451-00171c69419d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHx0cmFuc3BvcnR8ZW58MHx8fHwxNjM5NTY2NTg2&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z8tK50l_vloq9u1ev_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 25,
+//     "created": 1639760504792,
+//     "modified": 1639760504792,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "entertainment",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1499364615650-ec38552f4f34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHxlbnRlcnRhaW5tZW50fGVufDB8fHx8MTYzOTc2MDQ5Nw&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z8IHN6w_gQynsdQOr_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 26,
+//     "created": 1639983905450,
+//     "modified": 1639983905450,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "gas cylinder",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1511973774220-37fab967aba0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwyM3x8Z2FzJTIwY3lsaW5kZXJ8ZW58MHx8fHwxNjM5OTgzODY5&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z91W4OO_RVzwOhU1h_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 27,
+//     "created": 1639984231871,
+//     "modified": 1639984231871,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "pani jar",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1618513631742-747b74aa48f4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwxfHx3YXRlciUyMGphcnxlbnwwfHx8fDE2Mzk5ODQyMDI&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z91Xzvb_bUWTD3hdk_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   },
+//   {
+//     "id": 28,
+//     "created": 1639984350166,
+//     "modified": 1639984350166,
+//     "uid": "I2Rnkx0ONrRcmCLGeTyrsN2dwhu1",
+//     "categoryName": "lunch",
+//     "categoryDescription": "",
+//     "imageUrl": "https://images.unsplash.com/photo-1546793665-c74683f339c1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHNlYXJjaHwzfHxsdW5jaHxlbnwwfHx8fDE2Mzk5ODQzMTg&ixlib=rb-1.2.1&q=80&w=1080",
+//     "key": "z9208mK_MCMuDdpO7_I2Rnkx0ONrRcmCLGeTyrsN2dwhu1"
+//   }
+// ]
 
 
 const CategoriesContainer = () => {
+  const [categories, setCategories] = useState<category[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const { currentUser } = useAuth()
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        setLoading(true)
+        const res = await fetch(`https://serene-crag-89401.herokuapp.com/api/${currentUser?.uid}/expenseCategories/uid`)
+        const resData = await res.json()
+
+        if (res.ok) {
+          console.log(resData)
+          setCategories(resData?.expenseCategories)
+        }
+
+      }
+      catch (err) {
+        console.log(err)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCategories()
+
+
+  }, [currentUser])
 
   return (
     <div className="w-11/12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto py-5" >
-      {categories.map(category => {
+      {loading ? <p>
+        loading...
+      </p> : (
+        categories.map(category => {
 
-        return <Category key={category.id} category={category} />
+          return <Category key={category.id} category={category} />
 
-      })}
+        })
+      )}
     </div>
   )
 
